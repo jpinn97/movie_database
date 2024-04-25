789896ed9b25947c6e6f1e6507438c8a0eee00de
jpinn97 <JP878@canterbury.ac.uk>
2024-04-25 20:23:53 +0100

    Dynamic pathing

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import os
 
 # Create a connection to the database
 cnx = mysql.connector.connect(
@@ -15,6 +16,22 @@ for statement in sql.split(";"):
     if statement.strip() != "":
         cursor.execute(statement)
         cnx.commit()
+
+base_path = os.path.dirname(os.path.abspath(__file__))
+
+sql = sql.replace(
+    "csv/Country-table.csv", os.path.join(base_path, "csv/Country-table.csv")
+)
+sql = sql.replace("csv/Movie-table.csv", os.path.join(base_path, "csv/Movie-table.csv"))
+sql = sql.replace(
+    "csv/Artist-table.csv", os.path.join(base_path, "csv/Artist-table.csv")
+)
+sql = sql.replace("csv/Role-table.csv", os.path.join(base_path, "csv/Role-table.csv"))
+sql = sql.replace(
+    "csv/Internet-user-table.csv",
+    os.path.join(base_path, "csv/Internet-user-table.csv"),
+)
+
 
 with open("scripts/import_script.sql", "r") as file:
     sql = file.read()
--- scripts/import_script.sql
+++ scripts/import_script.sql
@@ -1,8 +1,8 @@
 USE movies_database;
 -- Disable foreign key checks
 SET FOREIGN_KEY_CHECKS = 0;
-LOAD DATA LOCAL INFILE '/home/jpinn/movie_database/csv/Country-table.csv' INTO TABLE Country FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES (code, name, language);
-LOAD DATA LOCAL INFILE '/home/jpinn/movie_database/csv/Movie-table.csv' INTO TABLE Movie FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES (
+LOAD DATA LOCAL INFILE 'csv/Country-table.csv' INTO TABLE Country FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES (code, name, language);
+LOAD DATA LOCAL INFILE 'csv/Movie-table.csv' INTO TABLE Movie FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES (
     movieId,
     title,
     year,
@@ -11,8 +11,8 @@ LOAD DATA LOCAL INFILE '/home/jpinn/movie_database/csv/Movie-table.csv' INTO TAB
     summary,
     countryCode
 );
-LOAD DATA LOCAL INFILE '/home/jpinn/movie_database/csv/Artist-table.csv' INTO TABLE Artist FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES (artistId, surname, name, DOB);
-LOAD DATA LOCAL INFILE '/home/jpinn/movie_database/csv/Role-table.csv' INTO TABLE Role FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES (movieId, actorId, roleName);
-LOAD DATA LOCAL INFILE '/home/jpinn/movie_database/csv/Internet-user-table.csv' INTO TABLE Internet_user FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES (email, surname, name, region);
+LOAD DATA LOCAL INFILE 'csv/Artist-table.csv' INTO TABLE Artist FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES (artistId, surname, name, DOB);
+LOAD DATA LOCAL INFILE 'csv/Role-table.csv' INTO TABLE Role FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES (movieId, actorId, roleName);
+LOAD DATA LOCAL INFILE 'csv/Internet-user-table.csv' INTO TABLE Internet_user FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES (email, surname, name, region);
 -- Enable foreign key checks
 SET foreign_key_checks = 1;
