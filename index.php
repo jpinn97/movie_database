f610cb36e39dfd2b2f81a38186d44db6506220ea
Rashid <ao411@canterbury.ac.uk>
2024-05-01 17:42:52 +0100

    Modify HTML layout and dynamic PHP functionality

--- index.php
+++ index.php
@@ -1,34 +1,43 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <title>Movie Database</title>
     <script src="https://unpkg.com/htmx.org"></script>
+    <link rel="stylesheet" href="style.css">
 </head>
-
 <body>
-
+<?php
+        echo "Hello, World! This is a PHP page running on XAMPP.";
+?>
     <h1>Movie Database</h1>
     <form>
         <label for="username">Username:</label><br>
         <input type="text" id="username" name="username"><br>
         <br>
         <label for="password">Password:</label><br>
-        <input type="text" id="password" name="password"><br>
+        <input type="password" id="password" name="password"><br>
         <br>
-
-        <button hx-post="/php/query_1.php" hx-trigger="click" hx-target="#result" hx-vals='{"username": document.getElementById("username").value, "password": document.getElementById("password").value}'>
-            Query 1
-        </button>
-        <button hx-post="/php/query_2.php" hx-trigger="click" hx-target="#result" hx-vals='{"username": document.getElementById("username").value, "password": document.getElementById("password").value}'>
-            Query 2
-        </button>
-        <button hx-post="/php/query_3.php" hx-trigger="click" hx-target="#result" hx-vals='{"username": document.getElementById("username").value, "password": document.getElementById("password").value}'>
-            Query 3
-        </button>
+        <button type="button" data-username="username" data-password="password" id="query1" hx-post="/php/query_1.php" hx-trigger="click" hx-target="#result">
+    Query 1  
+</button>
+<button type="button" data-username="username" data-password="password" id="query2" hx-post="/php/query_2.php" hx-trigger="click" hx-target="#result">
+    Query 2
+</button>
+<button type="button" data-username="username" data-password="password" id="query3" hx-post="/php/query_3.php" hx-trigger="click" hx-target="#result">
+    Query 3
+</button>
+<script>
+['query1', 'query2', 'query3'].forEach(function(buttonId) {
+    document.getElementById(buttonId).addEventListener('click', function() {
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        this.setAttribute('hx-vals', JSON.stringify({ username: username, password: password }));
+    });
+});
+</script>
     </form>
-
-    <div id="result" style="width: 200px; height: 200px; border: 1px solid black;"></div>
+    <div id="result" style="width: 800px; height: 400px; border: 2px solid black; overflow: auto;"></div>
 </body>
-
 </html>
+
+
