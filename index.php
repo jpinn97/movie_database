a01bb0b200649e7f1d06011b28197e1af0fd395f
Rashid <ao411@canterbury.ac.uk>
2024-05-04 10:23:41 +0100

    Modify UI and it's functionality

--- index.php
+++ index.php
@@ -3,41 +3,34 @@
 <head>
     <title>Movie Database</title>
     <script src="https://unpkg.com/htmx.org"></script>
-    <link rel="stylesheet" href="style.css">
+    <style>
+        button {
+            padding: 10px 20px; 
+            font-size: 16px; 
+            margin-bottom: 10px; 
+        }
+    </style>
 </head>
 <body>
-<?php
-        echo "Hello, World! This is a PHP page running on XAMPP.";
-?>
+    <?php echo "Hello, World! This is a PHP page running on XAMPP...."; ?>
     <h1>Movie Database</h1>
     <form>
         <label for="username">Username:</label><br>
-        <input type="text" id="username" name="username"><br>
-        <br>
+        <input type="text" id="username" name="username"><br><br>
         <label for="password">Password:</label><br>
-        <input type="password" id="password" name="password"><br>
-        <br>
-        <button type="button" data-username="username" data-password="password" id="query1" hx-post="/php/query_1.php" hx-trigger="click" hx-target="#result">
-    Query 1  
-</button>
-<button type="button" data-username="username" data-password="password" id="query2" hx-post="/php/query_2.php" hx-trigger="click" hx-target="#result">
-    Query 2
-</button>
-<button type="button" data-username="username" data-password="password" id="query3" hx-post="/php/query_3.php" hx-trigger="click" hx-target="#result">
-    Query 3
-</button>
-<script>
-['query1', 'query2', 'query3'].forEach(function(buttonId) {
-    document.getElementById(buttonId).addEventListener('click', function() {
-        var username = document.getElementById('username').value;
-        var password = document.getElementById('password').value;
-        this.setAttribute('hx-vals', JSON.stringify({ username: username, password: password }));
-    });
-});
-</script>
+        <input type="password" id="password" name="password"><br><br>
+
+        <!-- SQL Query Buttons -->
+        <button type="button" hx-post="/movie_database/php/query_1.php" hx-trigger="click" hx-target="#result" hx-vals='javascript:{"username": document.getElementById("username").value, "password": document.getElementById("password").value}'>SQL Query 1</button>
+        <button type="button" hx-post="/movie_database/php/query_2.php" hx-trigger="click" hx-target="#result" hx-vals='javascript:{"username": document.getElementById("username").value, "password": document.getElementById("password").value}'>SQL Query 2</button>
+        <button type="button" hx-post="/movie_database/php/query_3.php" hx-trigger="click" hx-target="#result" hx-vals='javascript:{"username": document.getElementById("username").value, "password": document.getElementById("password").value}'>SQL Query 3</button>
+
+        <!-- MongoDB Query Buttons -->
+        <button type="button" hx-post="/movie_database/mongo/query_1.php" hx-trigger="click" hx-target="#result" hx-vals='javascript:{"username": document.getElementById("username").value, "password": document.getElementById("password").value}'>MongoDB Query 1</button>
+        <button type="button" hx-post="/movie_database/mongo/query_2.php" hx-trigger="click" hx-target="#result" hx-vals='javascript:{"username": document.getElementById("username").value, "password": document.getElementById("password").value}'>MongoDB Query 2</button>
+        <button type="button" hx-post="/movie_database/mongo/query_3.php" hx-trigger="click" hx-target="#result" hx-vals='javascript:{"username": document.getElementById("username").value, "password": document.getElementById("password").value}'>MongoDB Query 3</button>
     </form>
-    <div id="result" style="width: 800px; height: 400px; border: 2px solid black; overflow: auto;"></div>
+
+    <div id="result" style="width: 1000px; height: 400px; border: 4px solid black; overflow: auto;"></div>
 </body>
 </html>
-
-
